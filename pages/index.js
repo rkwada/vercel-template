67f55f8fc22f8ef8f398f332c49f406a111456be
rkwada <rikuo.wada@gmail.com>
2025-10-20 10:47:46 -0600

    Added li name

--- pages/index.js
+++ pages/index.js
@@ -36,6 +36,9 @@ export default function Home() {
           <li className="tracking-[-.01em]">
             Save and see your changes instantly.
           </li>
+          <li className="mt-2 tracking-[-.01em] font-bold">
+            By Rikuo.
+          </li>
         </ol>
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
